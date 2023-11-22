4ed1de22b1e2e8fc2c7402ae13911a9a010e0e0c
Athirson Silva <Athirsonarceus@gmail.com>
2023-11-22 19:59:46 -0300

    initial commit

---
+++ src/main/java/com/asilva/springbootcleanarchitecture/SpringBootCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.asilva.springbootcleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCleanArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asilva/springbootcleanarchitecture/SpringBootCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asilva.springbootcleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
